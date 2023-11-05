47b4f3a528a6fa30b6d5778734ef361ba1209d14
apikmeister <afiq.mohamad90@gmail.com>
2023-11-05 10:15:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.jetpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Article()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Banner() {
+    Image(
+        painter = painterResource(id = R.drawable.bg_compose_background),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun Article(modifier: Modifier = Modifier) {
+    Column {
+        Banner()
+        Text(
+            stringResource(R.string.title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            stringResource(R.string.summary),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            stringResource(R.string.description),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTutorialTheme {
+        Article()
+    }
+}
